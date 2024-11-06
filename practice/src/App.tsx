19fb6a57cd801bfea690a2e79ee3e99de7ebca51
leejuhi <163210518+leejuhi@users.noreply.github.com>
2024-11-06 15:01:03 +0900

    feature: 활동페이지 추가
    그냥 페이지만 추가

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -5,6 +5,7 @@ import HomePage from './components/pages/HomePage.tsx';
 import LoginPage from './components/pages/LoginPage.tsx';
 import RecruitPage from './components/pages/RecruitPage.tsx';
 import QnaPage from './components/pages/QnAPage.tsx';
+import ActivityPage from './components/pages/ActivityPage.tsx';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/recruit" element={<RecruitPage />} />
             <Route path="/qna" element={<QnaPage />} />
+            <Route path="/activity" element={<ActivityPage />} />
           </Routes>
         </BrowserRouter>
     </>
---
+++ practice/src/components/pages/ActivityPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../navBar/navBar.tsx";
+
+
+const ActivityPage = () => {
+
+
+    return (
+       <>
+            <NavBar/>
+       </>
+    );
+};
+
+export default ActivityPage;
