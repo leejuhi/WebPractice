3cd41892ee2784c67998a14384b9cba7a77b216c
leejuhi <163210518+leejuhi@users.noreply.github.com>
2024-11-06 13:06:47 +0900

    chore: 폴더 및 tsx 파일 정리
    빈 파일

---
+++ practice/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyle from './asset/font/GlobalStyle';
+
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+
+          </Routes>
+        </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
