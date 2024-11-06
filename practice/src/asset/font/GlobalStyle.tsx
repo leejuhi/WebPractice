6b2b4c4f727d4b9e756d1d62a7ad60c476f74ed5
leejuhi <163210518+leejuhi@users.noreply.github.com>
2024-11-06 12:41:40 +0900

    style: 폰트 설정
    폰트 설정

---
+++ practice/src/asset/font/GlobalStyle.tsx
@@ -0,0 +1,36 @@
+import { Global, css } from "@emotion/react";
+import fonts from "./font.ts"; // font.ts 파일의 설정을 가져옴
+import React from "react";
+
+const GlobalStyle = () => (
+    <Global
+        styles={css`
+            ${fonts}
+
+            body {
+                font-family: 'Noto Sans KR', sans-serif;
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+
+            input:focus {
+                outline: none;
+                border-bottom: 1px solid #037ffc;
+            }
+            a {
+                text-decoration: none;
+                color: inherit;
+            }
+
+            a:visited {
+                color: inherit;
+            }
+            button{
+                border: none;
+            }
+        `}
+    />
+);
+
+export default GlobalStyle;
---
+++ practice/src/asset/font/font.ts
@@ -0,0 +1,7 @@
+import { css } from "@emotion/react";
+
+const fonts = css`
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
+`;
+
+export default fonts;
